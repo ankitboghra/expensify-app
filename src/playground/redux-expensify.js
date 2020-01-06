514f3a55f0db0d4295fa3c506b697b9855983624
Ankitkumar Boghra <ankitboghra@gmail.com>
2020-01-06 14:46:49 +0530

    completing redux base

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -28,6 +28,35 @@ const removeOption = ({ id } = {}) =>({
     id
 });
 
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
+const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+});
+
+const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+});
+
+const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
+
+const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+});
+
 //
 // Reducers
 //
@@ -48,6 +77,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
 
         case 'REMOVE_EXPENSE':
             return state.filter(({id}) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if(expense.id === action.id){
+                    return {
+                        ...expense,
+                        ...action.updates
+                    }
+                } else {
+                    return state;
+                }
+            })
         default:
             return state;
     }
@@ -55,6 +95,31 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch(action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text:action.text
+            };
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            };
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            };
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            };
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                endDate: action.endDate
+            };
         default:
             return state;
     }
@@ -76,9 +141,19 @@ store.subscribe(() => {
     }
 );
 
-const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 200}));
-const expenseTwo = store.dispatch(addExpense({description: 'Tea', amount: 300}));
+// const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 200}));
+// const expenseTwo = store.dispatch(addExpense({description: 'Tea', amount: 300}));
 
-store.dispatch(
-    removeOption({id: expenseOne.expense.id})
-);
+// store.dispatch(removeOption({id: expenseOne.expense.id}));
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+// store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter());
+
+// store.dispatch(sortByDate());
+// store.dispatch(sortByAmount());
+
+store.dispatch(setStartDate(123));
+store.dispatch(setStartDate());
+store.dispatch(setEndDate(345));
+store.dispatch(setEndDate());
