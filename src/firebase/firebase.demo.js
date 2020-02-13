027721bff807abe7ed395b4a67ce6ed519c79a63
Ankitkumar Boghra <ankitboghra@gmail.com>
2020-02-13 15:53:42 +0530

    trying out promises with firebase

--- src/firebase/firebase.demo.js
+++ src/firebase/firebase.demo.js
@@ -10,4 +10,35 @@ const firebaseConfig = {
     appId: ""
   };
 
-firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
+  
+  const database =  firebase.database();
+  
+  database.ref().set({
+      name: 'Ankit Boghra',
+      age: 23,
+      location: {
+        city: 'Surat',
+        country: 'India'
+      }
+  })
+  .then(() => {
+    console.log('Data saved successfully!');
+  })
+  .catch((e) => {
+    console.log('Something went wrong! ', e);
+  })
+  ;
+  
+  database.ref('location/city').set('Bangalore');
+  
+  database.ref('attributes').set({
+    height: 5.8,
+    weight: 69.9
+  })
+  .then(() => {
+    console.log('Data updated successfully!');
+  })
+  .catch((e) => {
+    console.log('Something went wrong while updating! ', e);
+  });
