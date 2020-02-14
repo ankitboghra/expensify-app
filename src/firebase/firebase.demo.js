b19507bb4d5c62df0b0b66ab815ae2190d1450fd
Ankitkumar Boghra <ankitboghra@gmail.com>
2020-02-14 15:13:46 +0530

    firebase crud operations

--- src/firebase/firebase.demo.js
+++ src/firebase/firebase.demo.js
@@ -17,6 +17,8 @@ const firebaseConfig = {
   database.ref().set({
       name: 'Ankit Boghra',
       age: 23,
+      isSingle: true,
+      job: 'Software Developer',
       location: {
         city: 'Surat',
         country: 'India'
@@ -30,15 +32,61 @@ const firebaseConfig = {
   })
   ;
   
+  // Updating data
   database.ref('location/city').set('Bangalore');
   
-  database.ref('attributes').set({
-    height: 5.8,
-    weight: 69.9
-  })
-  .then(() => {
-    console.log('Data updated successfully!');
-  })
-  .catch((e) => {
-    console.log('Something went wrong while updating! ', e);
+  database.ref('attributes')
+    .set({
+      height: 5.8,
+      weight: 69.9
+    })
+    .then(() => {
+      console.log('Data updated successfully!');
+    })
+    .catch((e) => {
+      console.log('Something went wrong while updating! ', e);
   });
+  
+  // Removing data, Way 1
+  database.ref('isSingel')
+    .remove()
+    .then(()=> {
+      console.log('Data removed successfully!');
+    })
+    .catch((e_ => {
+      console.log('Something went wrong while removing data!');
+  }));
+  
+  // Removing data, Way 2
+  
+  database.ref('age').set(null);
+  
+  // Updating or deleting or adding data
+  database.ref().update({
+    name: 'Ankit B',
+    age: 22,
+    isSingle: null,
+    'location/city': 'Bengaluru'
+  });
+  
+  // Fetching data, Way 1
+  database.ref().once('value')
+    .then((snapshot) => {
+      const val = snapshot.val();
+      console.log(val);
+    })
+    .catch((e) => {
+      console.log('Some thing went wrong while fetching data!')
+    });
+  
+  
+  // Fetching data, Way 1
+  
+  const onValueChange = database.ref().on('value', (snapshot) => {
+    console.log(snapshot.val());
+  });
+  
+  setTimeout(() => {
+    database.ref().off('value', onValueChange);
+  }, 3500);
+  
