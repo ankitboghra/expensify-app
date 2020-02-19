117ff16d20936832bcab1ba03dae4f4f50f73af0
Ankitkumar Boghra <ankitboghra@gmail.com>
2020-02-19 17:23:06 +0530

    integrating edit and delete functionality

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,20 +3,20 @@ import queryString from 'query-string';
 import { connect } from 'react-redux';
 
 import ExpenseForm from './ExpenseForm';
-import { editExpense, removeExpense } from '../actions/expenses';
+import { startEditExpense, startRemoveExpense } from '../actions/expenses';
 
 const EditExpensePage = (props) => (
     <div>
         <ExpenseForm
             expense={props.expense}
             onSubmit={(expense) =>  {
-                props.dispatch(editExpense(props.expense.id, expense));
+                props.dispatch(startEditExpense(props.expense.id, expense));
                 props.history.push('/');
             }
             }
         />
         <button onClick={() => {
-            props.dispatch(removeExpense({id: props.expense.id}))
+            props.dispatch(startRemoveExpense({id: props.expense.id}))
             props.history.push('/');
         }}>Remove</button>
     </div>
