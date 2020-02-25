dec5966e5f713d24ed43ea710b6aed253bf98349
Ankitkumar Boghra <ankitboghra@gmail.com>
2020-02-25 21:13:43 +0530

    adding auth redirection

--- src/app.js
+++ src/app.js
@@ -5,10 +5,11 @@ import { firebase } from './firebase/firebase';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 
 const store = configureStore();
 
@@ -17,17 +18,28 @@ const jsx = (
         <AppRouter />
     </Provider>
 );
+let hasRendered = false;
+const renderApp = () => {
+    if(!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('appRoot'));
+        hasRendered = true;
+    }
+};
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('appRoot'));
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('appRoot'));
-});
-
 firebase.auth().onAuthStateChanged((user) => {
     if(user) {
-        console.log('Logged In');
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if(history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });
     } else {
-        console.log('Logged Out');
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
     }
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,6 @@ const Header = ({ startLogout }) => (
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
         <button onClick={startLogout}>LogOut</button>
     </header>
 );
