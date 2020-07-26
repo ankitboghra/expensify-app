e10991c7a3458c77b9e747c822c37d3f91527551
ankitboghra <ankitboghra@gmail.com>
2020-07-26 22:42:39 +0530

    adding expense summary functionality

---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expensesTotal';
+
+const ExpensesSummary = ({expenseCount, expensesTotal}) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpenseTotal = numeral(expensesTotal).format('Rs0,0.00');
+    return (
+        <div>
+            <h1>Viewing {expenseCount} {expenseWord} totalling Rs {formattedExpenseTotal}</h1>
+        </div>
+    );
+};
+
+const mapStateToProps = (state) => {
+    const visibleExpenses = selectExpenses(state.expenses, state.filters);
+
+    return {
+        expenseCount: visibleExpenses.length,
+        expensesTotal: selectExpensesTotal(visibleExpenses)
+    };
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
